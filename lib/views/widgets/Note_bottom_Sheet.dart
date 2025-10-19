a2f8ff4ef35758abfe14fb4777c83974f3381d4f
???? ??? <????? ?????????? ???>
2025-10-19 23:19:36 +0300

    finish note bottom sheet

--- lib/views/widgets/Note_bottom_Sheet.dart
+++ lib/views/widgets/Note_bottom_Sheet.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app2/constants.dart';
+import 'package:notes_app2/custom_widget/custom_bottom.dart';
 import 'package:notes_app2/custom_widget/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,15 +11,23 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(children: [
-        SizedBox(height: 33,)
-        ,CustomTextField(hint: 'title',)
-       ,SizedBox(height: 16,)
-        ,CustomTextField(
-        maxLine: 5
-        ,hint: 'content',)
+      child: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(height: 33,)
+          ,CustomTextField(hint: 'title',)
+         ,SizedBox(height: 24,)
+          ,CustomTextField(
+          maxLine: 7
+          ,hint: 'content',),
 
-      ],),
+        Padding(
+          padding: const EdgeInsets.only(top:90,bottom:50 ),
+          child: CustomBottom(),
+        )
+        
+        ],),
+      ),
     );
   }
 }
+
