0dfc8aec1b1fcef7842ab6aa57348e5dfe7adaed
???? ??? <????? ?????????? ???>
2025-10-19 21:16:12 +0300

    creat custom textField

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app2/views/widgets/Note_bottom_Sheet.dart';
 import 'package:notes_app2/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,19 +9,12 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
     floatingActionButton:   FloatingActionButton(onPressed: (){
-      showModalBottomSheet(context: context, builder: (context){
+      showModalBottomSheet(shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16)
+      ),context: context, builder: (context){
        return  const AddNoteBottomSheet();
       });
     },child: Icon(Icons.add),)
     ,body: NotesViewBody(),);
   }
 }
-
- class AddNoteBottomSheet extends StatelessWidget {
-   const AddNoteBottomSheet({super.key});
-
-   @override
-   Widget build(BuildContext context) {
-     return  Container();
-   }
- }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:notes_app2/custom_app_bar/custom_app_bar.dart';
+import 'package:notes_app2/custom_widget/custom_app_bar.dart';
 import 'package:notes_app2/views/widgets/Nots_view_list.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -19,36 +19,6 @@ class NotesViewBody extends StatelessWidget {
           Expanded(child: NotsListView()),
         ],
       ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text("Flutter Tips",style: TextStyle(fontSize: 30,color: Colors.black),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text("Build Your Career With Tharwat Samy",style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5))),
-            ),
-            trailing: IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.trash,color: Colors.black,size: 24,),),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text("18oct.2025",style:TextStyle(fontSize: 15,color: Colors.black.withOpacity(.5))),
-          )
-        ],
-      ),
-      decoration: BoxDecoration(
-          color: Color(0xffFFcE80), borderRadius: BorderRadius.circular(16)),
     );
   }
 }
