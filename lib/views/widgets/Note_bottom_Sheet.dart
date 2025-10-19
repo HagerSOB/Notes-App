308e02997b115dae6aa47138b9e4c7f1316d35ab
???? ??? <????? ?????????? ???>
2025-10-19 22:49:45 +0300

    add field to bottom sheet

---
+++ lib/views/widgets/Note_bottom_Sheet.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app2/custom_widget/custom_text_field.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [
+        SizedBox(height: 33,)
+        ,CustomTextField(hint: 'title',)
+       ,SizedBox(height: 16,)
+        ,CustomTextField(
+        maxLine: 5
+        ,hint: 'content',)
+
+      ],),
+    );
+  }
+}
