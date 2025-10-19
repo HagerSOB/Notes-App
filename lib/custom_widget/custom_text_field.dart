31a8acb0af0ad740be8e81ce174f8502add235bd
???? ??? <????? ?????????? ???>
2025-10-19 22:50:56 +0300

    add field to bottom sheet

---
+++ lib/custom_widget/custom_text_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app2/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint, this.maxLine=1});
+final String hint;
+final int maxLine;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+
+      cursorColor:KPrimaryColor,
+maxLines: maxLine,
+      decoration: InputDecoration(
+        hintText: hint,
+          border: bulidBorder(),
+              focusedBorder: bulidBorder( KPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder bulidBorder([color]) {
+    return OutlineInputBorder(
+borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color:  color??Colors.white
+      )
+);
+  }
+}
