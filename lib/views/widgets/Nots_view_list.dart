9e6f56ff92950b2be723b89d9212abcc0405fcdb
???? ??? <????? ?????????? ???>
2025-10-22 00:56:20 +0300

    create type adaptor for Note_modle

---
+++ lib/views/widgets/Nots_view_list.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app2/views/Edit_View_Body.dart';
+import 'package:notes_app2/views/widgets/Edit_Note_View.dart';
+import 'package:notes_app2/views/widgets/note_item.dart';
+import 'package:notes_app2/views/widgets/notes_view_body.dart';
+
+class NotsListView extends StatelessWidget {
+  const NotsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+          padding: EdgeInsets.zero
+          ,itemBuilder: (context,index){
+        return  Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: GestureDetector(
+              child: NoteItem(),
+          onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return EditNoteView();
+                }));
+          },
+          ),
+        );
+      }),
+    );
+  }
+}
