3d17e03e4450e6c3ca61180dc01e4ab4276f5506
???? ??? <????? ?????????? ???>
2025-10-25 03:24:16 +0300

    Add Note Method

--- lib/add_note_cubit/add_note_cubit.dart
+++ lib/add_note_cubit/add_note_cubit.dart
@@ -1,11 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app2/Models/Note_modle.dart';
 import 'package:notes_app2/add_note_cubit/notes_state.dart';
+import 'package:notes_app2/constants.dart';
 
 
 class AddNoteCubit extends Cubit<NotesState> {
   AddNoteCubit(super.Noteinitial);
- addNote(NoteModle note){}
+ addNote(NoteModle note) async{
+   emit(NoteLoading());
+    try{
+      var notesBox=Hive.box<NoteModle>(kNotesBox);
+      await notesBox.add(note);
+      emit(NoteSuccsses());
+
+    }catch (e){
+      emit(NoteFailure(e.toString()));
+    }
+
+ }
 
   }
 
